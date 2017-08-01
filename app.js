78c9874dc2b87e2409441edc4e6e208a2386f788
Mukesh Sharma <mukeshsharma@babydestination.com>
2017-08-01 19:04:33 +0530

    Added Package and Eslint

---
+++ app.js
@@ -0,0 +1,18 @@
+const express =  require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+
+const app = express();
+
+app.set("port", (process.env.PORT || 5000));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(logger("dev"));
+
+
+app.listen(app.get("port"), () => {
+  console.log('Node app is running on port', app.get("port")); // eslint-disable-line
+});
+
+module.exports = app;
